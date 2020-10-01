2bab163a5f139b02d42e953f9f5733c53f879dee
Josef Zoller <josef@walterzollerpiano.com>
2020-10-01 21:53:17 +0200

    Make some macros usable in Swift

--- Sources/cGLFW/GLFW.h
+++ Sources/cGLFW/GLFW.h
@@ -1,1 +1,13 @@
 #include <GLFW/glfw3.h>
+
+#ifdef GLFW_RESIZE_NESW_CURSOR
+#define _GLFW_RESIZE_NESW_CURSOR true
+#else
+#define _GLFW_RESIZE_NESW_CURSOR false
+#endif
+
+#ifdef GLFW_MOUSE_PASSTHROUGH
+#define _GLFW_MOUSE_PASSTHROUGH true
+#else
+#define _GLFW_MOUSE_PASSTHROUGH false
+#endif
