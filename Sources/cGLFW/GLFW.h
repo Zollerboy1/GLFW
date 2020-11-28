21325f2902b3bc49741c61912ac7c8bb19895e56
Josef Zoller <josef@walterzollerpiano.com>
2020-11-28 19:08:38 +0100

    Enable native access functions for macOS and Windows

--- Sources/cGLFW/GLFW.h
+++ Sources/cGLFW/GLFW.h
@@ -1,13 +1,23 @@
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+    #define GLFW_EXPOSE_NATIVE_WIN32
+#elif __APPLE__
+    #include <TargetConditionals.h>
+    #if TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE
+        #error "iOS is currently not supported!"
+    #elif TARGET_OS_MAC
+        #define GLFW_EXPOSE_NATIVE_COCOA
+    #else
+        #error "Unknown Apple platform!"
+    #endif
+#elif __linux__
+    #error "Linux is currently not supported!"
+#elif __unix__
+    #error "Unix platforms other than macOS are currently not supported!"
+#elif defined(_POSIX_VERSION)
+    #error "Posix platforms are currently not supported!"
+#else
+    #error "Unknown compiler!"
+#endif
+
 #include <GLFW/glfw3.h>
-
-#ifdef GLFW_RESIZE_NESW_CURSOR
-#define _GLFW_RESIZE_NESW_CURSOR true
-#else
-#define _GLFW_RESIZE_NESW_CURSOR false
-#endif
-
-#ifdef GLFW_MOUSE_PASSTHROUGH
-#define _GLFW_MOUSE_PASSTHROUGH true
-#else
-#define _GLFW_MOUSE_PASSTHROUGH false
-#endif
+#include <GLFW/glfw3native.h>
